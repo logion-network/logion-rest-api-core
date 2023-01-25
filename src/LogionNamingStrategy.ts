2bb0fec2bdf997f9feb9341270d35bb828240e97
Benoit Devos <benoit@logion.network>
2023-01-25 16:10:17 +0100

    chore: limit DB constraints and index names to 63 chars.

--- src/LogionNamingStrategy.ts
+++ src/LogionNamingStrategy.ts
@@ -1,5 +1,7 @@
 import { DefaultNamingStrategy, Table } from "typeorm";
 
+const MAX_NAME_LENGTH = 63;
+
 export class LogionNamingStrategy extends DefaultNamingStrategy {
 
     primaryKeyName(tableOrName: Table | string): string {
@@ -13,16 +15,19 @@ export class LogionNamingStrategy extends DefaultNamingStrategy {
 
     uniqueConstraintName(tableOrName: Table | string, columnNames: string[]): string {
         const tableName = this.actualTableName(tableOrName);
-        return columnNames.reduce((name, column) => `${ name }_${ column }`, `UQ_${ tableName }`);
+        return columnNames.reduce((name, column) => `${ name }_${ column }`, `UQ_${ tableName }`)
+            .substring(0, MAX_NAME_LENGTH);
     }
 
     foreignKeyName(tableOrName: Table | string, columnNames: string[]): string {
         const tableName = this.actualTableName(tableOrName);
-        return columnNames.reduce((name, column) => `${ name }_${ column }`, `FK_${ tableName }`);
+        return columnNames.reduce((name, column) => `${ name }_${ column }`, `FK_${ tableName }`)
+            .substring(0, MAX_NAME_LENGTH);
     }
 
     indexName(tableOrName: Table | string, columnNames: string[]): string {
         const tableName = this.actualTableName(tableOrName);
-        return columnNames.reduce((name, column) => `${ name }_${ column }`, `IX_${ tableName }`);
+        return columnNames.reduce((name, column) => `${ name }_${ column }`, `IX_${ tableName }`)
+            .substring(0, MAX_NAME_LENGTH);
     }
 }
